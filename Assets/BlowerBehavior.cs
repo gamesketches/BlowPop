90f1573e4eedb7ad9aae09320df13001e92c96c5
gamesketches <sam.von.ehren@gmail.com>
2021-02-15 21:24:09 -0500

    Blowing bubbles and shootin 'em out

--- Assets/BlowerBehavior.cs
+++ Assets/BlowerBehavior.cs
@@ -8,24 +8,36 @@ public class BlowerBehavior : MonoBehaviour
 	GameObject curBubble;
 	public AnimationCurve breath;
 	float breathTimer;
+	public float releaseThreshold;
+	float releaseTimer;
+	Rigidbody2D rigidbody;
     // Start is called before the first frame update
     void Start()
     {
         breathTimer = 0;
+		rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+		releaseTimer -= Time.deltaTime;
         if(Input.GetMouseButton(0)) {
 			if(curBubble) {
-				breathTimer += Time.deltaTime;
-				BlowUpBubble();
+				if(releaseTimer > 0) {
+					ReleaseBubble();
+					
+				} else {
+					breathTimer += Time.deltaTime;
+					BlowUpBubble();
+				}
 			} else {
 				curBubble = Instantiate<GameObject>(bubblePrefab);
 				curBubble.transform.parent = transform;
-				curBubble.transform.localPosition = Vector3.zero + (Vector3.up * 0.34f);
+				curBubble.transform.localPosition = Vector3.zero + (Vector3.up * 0.2f);
 			}
+		} else if(Input.GetMouseButtonUp(0) && curBubble) {
+			releaseTimer = releaseThreshold;
 		} else {
 			if(curBubble) {
 				breathTimer -= Time.deltaTime;
@@ -38,5 +50,14 @@ public class BlowerBehavior : MonoBehaviour
 		float curScalar = curBubble.transform.localScale.x;
 		curScalar = breath.Evaluate(breathTimer);
 		curBubble.transform.localScale = new Vector3(curScalar, curScalar, curScalar);
+	}
+
+	void ReleaseBubble() {
+		curBubble.transform.parent = null;
+		Vector3 movementVector = curBubble.transform.position - transform.position;
+		curBubble.GetComponent<Rigidbody2D>().AddForce(movementVector, ForceMode2D.Impulse);
+		rigidbody.AddForce(-movementVector, ForceMode2D.Impulse);
+		curBubble = null;
+		releaseTimer = 0;
 	}
 }
