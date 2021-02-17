27bfde6faf56e407b855c0a24cb4ae344be709e2
sethHex <seth@hexagram.io>
2021-02-16 19:06:53 -0500

    Creating Bubble Logic

--- Assets/Scripts/BubbleBehavior.cs
+++ Assets/Scripts/BubbleBehavior.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class BubbleBehavior : MonoBehaviour
 {
+    Rigidbody2D rb;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Blower") {
+
+        }
+
+        if (collision.gameObject.tag == "Bubble")
+        {
+            HingeJoint2D myJoint = (HingeJoint2D)gameObject.AddComponent<HingeJoint2D>();
+            myJoint.connectedBody = gameObject.GetComponent<Rigidbody2D>();
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
