bc0cb5d039f239b7a260c9dcdfa5027a92d3c73b
gamesketches <sam.von.ehren@gmail.com>
2021-02-17 14:59:53 -0500

    Adding in enemy generator code...def needs tuning

--- Assets/Scripts/EnemyGenerator.cs
+++ Assets/Scripts/EnemyGenerator.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class EnemyGenerator : MonoBehaviour
 {
+	public float spawnInterval;
+	public float spawnDistanceScalar;
+	float spawnTimer;
+	public Transform background;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnTimer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        spawnTimer += Time.deltaTime;
+		if(spawnTimer > spawnInterval) {
+			CreateEnemy();
+			spawnTimer = 0;
+		}
     }
+
+	void CreateEnemy() {
+		GameObject newEnemy = Instantiate<GameObject>(Resources.Load<GameObject>("Prefabs/Tooth"));
+		newEnemy.transform.parent = background.transform;
+		newEnemy.transform.localPosition = Vector3.zero;
+		newEnemy.transform.Rotate(0, 0, Random.value * 360);
+		newEnemy.transform.position = newEnemy.transform.up.normalized * spawnDistanceScalar;
+		newEnemy.transform.parent = null;
+		newEnemy.GetComponent<ToothBehavior>().MoveTowardsCenter();
+	}
 }
---
+++ Assets/ToothBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToothBehavior : MonoBehaviour
+{
+	public float timeToCenter;
+	Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void MoveTowardsCenter() {
+		StartCoroutine(MoveTowardsPoint(Vector3.zero));
+	}
+
+	IEnumerator MoveTowardsPoint(Vector3 targetPoint) {
+		Vector3 startPos = transform.position;
+		for(float t = 0; t < timeToCenter; t+= Time.deltaTime) {
+			Vector3 newPos = Vector3.Lerp(startPos, targetPoint, t / timeToCenter);
+			rigidbody.MovePosition(new Vector2(newPos.x, newPos.y));
+			yield return null;
+		}
+	}
+}
