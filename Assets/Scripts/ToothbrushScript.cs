e238f4e3879d9271fdc7888231769d3ed2f5d89a
sethHex <seth@hexagram.io>
2021-03-08 10:55:29 -0500

    Updated 2 different brush lengths

--- Assets/Scripts/ToothbrushScript.cs
+++ Assets/Scripts/ToothbrushScript.cs
@@ -6,6 +6,7 @@ public class ToothbrushScript : MonoBehaviour
 {
     float randomizer;
     Animator anim;
+    bool setAnim;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,15 +21,30 @@ public class ToothbrushScript : MonoBehaviour
     {
         randomizer -= Time.deltaTime;
 
-        if (randomizer <= 0) {
+        if (randomizer <= 0 && !setAnim) {
+            float rando = Random.Range(0, 2);
+            if (rando == 1)
+            {
+                anim.SetBool("short", true);
+            }
+            else
+            {
+                anim.SetBool("long", true);
+            }
             anim.enabled = true;
             transform.GetChild(0).gameObject.SetActive(true);
+            setAnim = true;
         }
     }
 
     public void ResetAnim() {
         randomizer = Random.Range(5.0f, 15.0f);
         anim.enabled = false;
+        anim.SetBool("long", false);
+        anim.SetBool("short", false);
         transform.GetChild(0).gameObject.SetActive(false);
+        setAnim = false;
     }
+
+   
 }
