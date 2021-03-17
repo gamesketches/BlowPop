08a67852c3a6d13045f7a50bfca919151e452ab6
gamesketches <sam.von.ehren@gmail.com>
2021-03-17 19:05:25 -0400

    Adding some basic sound effects to title screen

--- Assets/Scripts/TitleMenu.cs
+++ Assets/Scripts/TitleMenu.cs
@@ -9,10 +9,12 @@ public class TitleMenu : MonoBehaviour
 	public string[] options;
 	int cursorPosition;
 	Transform cursor;
+	AudioSource audio;
 
     // Start is called before the first frame update
     void Start()
     {
+		audio = GetComponent<AudioSource>();
         cursorPosition = 0;
 		cursor = transform.GetChild(options.Length);
 		if(options.Length != transform.childCount - 1) Debug.LogError("Number of options is insufficient!");
@@ -26,13 +28,15 @@ public class TitleMenu : MonoBehaviour
     {
 		if(Input.GetKeyDown(KeyCode.DownArrow)) {
 			cursorPosition = (cursorPosition + 1) % options.Length;
+			audio.Play();
 		}
 		else if(Input.GetKeyDown(KeyCode.UpArrow)) {
 			cursorPosition--;
 			if(cursorPosition < 0) cursorPosition = options.Length - 1;
+			audio.Play();
 		}
 		UpdateCursorPosition();
-		if(Input.GetKeyDown(KeyCode.Space)) {
+		if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return)) {
 			ProcessMenuSelection();
 		}
     }
@@ -43,10 +47,17 @@ public class TitleMenu : MonoBehaviour
 		cursor.position = curCursorPosition;
 	}
 
+	IEnumerator BeginGame() {
+		audio.clip = Resources.Load<AudioClip>("Sound/Confirm");
+		audio.Play();
+		while(audio.isPlaying) yield return null;
+		SceneManager.LoadScene(0);
+	}
+
 	void ProcessMenuSelection() {
 		switch(cursorPosition) {
 			case 0: 
-				SceneManager.LoadScene(0);
+				StartCoroutine(BeginGame());
 				break;
 			case 1:
 				Debug.Log("print life");
